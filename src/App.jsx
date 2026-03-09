81b2d51469fa14b71c0d53403221bf7062551082
Stonewall19 <munyamatose777@gmail.com>
2026-03-09 12:25:11 +0200

    Create Marketplace component in App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Marketplace = () => {
+    return (
+        <div>
+            <h1>Welcome to the StoneWall Marketplace</h1>
+            <p>Explore a wide variety of products.</p>
+            <div className="product-grid">
+                {/* Product items would be rendered here */}
+            </div>
+        </div>
+    );
+};
+
+export default Marketplace;
